d447f398039f18c3ef88478a04771055ae0091e9
Raian Tasnim <raiantasnim2411@gmail.com>
2026-01-28 11:13:04 +0600

    made navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import logo from "../assets/logo.png"
 export const Header = () => {
   return (
-    <header className="w-full bg-slate-900 text-white">
+    <header className="w-full bg-[#6F4E37] text-slate-900">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         
         {/* Logo */}
-        <div className="text-2xl font-bold tracking-wide">
-          Brand
+        <div className="text-2xl font-bold rounded-xl tracking-wide cursor-pointer ">
+           <img
+             src={logo} 
+             alt="Brand logo" 
+             className="h-15 w-auto  rounded-xl object-contain" 
+           />
         </div>
 
         {/* Navigation */}
@@ -27,7 +31,7 @@ export const Header = () => {
         </nav>
 
         {/* Button */}
-        <button className="hidden md:block bg-sky-500 hover:bg-sky-600 px-5 py-2 rounded-lg text-sm font-medium transition">
+        <button className="hidden md:block bg-[#F5F5DC] hover:bg-[#bd4e0eb8] px-5 py-2 rounded-lg text-sm text-[#6f4a37b8] hover:text-[#F5F5DC] font-medium transition">
           Sign In
         </button>
 
